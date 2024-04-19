84e7f7a6c3b39a9e0dc70df06e71cbe9e45025a4
microily <93529877+microily@users.noreply.github.com>
2024-04-19 23:24:00 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.picker import MDTimePicker
+
+# Описание интерфейса в формате Kivy
+kv = '''
+<MyApp>:
+    MDRaisedButton:
+        text: "Выбрать время"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.6}
+        on_release: app.show_time_picker()
+    MDLabel:
+        id: time_label
+        text: "Выбранное время: "
+        pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+'''
+
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_string(kv)
+
+    def show_time_picker(self):
+        time_dialog = MDTimePicker()
+        time_dialog.bind(time=self.set_time)
+        time_dialog.open()
+
+    def set_time(self, instance, time):
+        self.root.ids.time_label.text = "Выбранное время: " + str(time)
+
+if __name__ == "__main__":
+    MyApp().run()
